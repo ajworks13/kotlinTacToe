8fda0234b1ad3ca0a7db9e57c579faf693e304ff
AJ <63066039+ajworks13@users.noreply.github.com>
2022-09-30 07:26:52 +0000

    Update Main.kt
    gave the cpu a random name selector in the beginning

--- Main.kt
+++ Main.kt
@@ -1,13 +1,31 @@
 fun main() {
-    //var user = readLine()
+
+    var boss: String? = null // empty for now
+
+    val randomNumberForBoss = (1..3).random()
+    if(randomNumberForBoss == 1){
+        boss = "Master Donut Toe"
+    }else if(randomNumberForBoss == 2){
+        boss = "Blue Toe"
+    }else if(randomNumberForBoss == 3){
+        boss = "Thirty Toe"
+    }
+
+    println("Welcome user! CPU boss is waiting! what is your name?")
+    val user = readLine()
+    println("Alright $user, the boss you're facing this time is: $boss")
     gameBoard()
 
 } // main function
 
 fun gameBoard(p1: String = "", p2: String = ""){
+
     var firstRow = mutableListOf(" "," "," ")
     var secondRow = mutableListOf(" "," "," ")
     var thirdRow = mutableListOf(" "," ", " ")
+
+
+
 
     println(" ${firstRow[0]} | ${firstRow[1]} | ${firstRow[2]} ")
     println("-----------")

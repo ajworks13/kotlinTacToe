775a0b62bd874536c9a850a38dfcc72a4a4247c6
AJ <63066039+ajworks13@users.noreply.github.com>
2022-09-30 07:40:47 +0000

    Update Main.kt
    Decided to just do player vs cpu instead of player vs player. Maybe I will add it later after it is completed with cpu functionality.

--- Main.kt
+++ Main.kt
@@ -1,6 +1,8 @@
 fun main() {
 
     var boss: String? = null // empty for now
+    var playerDecision: String? = null
+    var gameOver = false
 
     val randomNumberForBoss = (1..3).random()
     if(randomNumberForBoss == 1){
@@ -14,16 +16,25 @@ fun main() {
     println("Welcome user! CPU boss is waiting! what is your name?")
     val user = readLine()
     println("Alright $user, the boss you're facing this time is: $boss")
-    gameBoard()
+    
+    do{
+        gameBoard()
+        println("$user, you go first!")
+        playerDecision = readLine()
+        
+        gameBoard(p1 = playerDecision)
+        // LEFT OFF HERE.....................................
+    }while(gameOver != true)
 
 } // main function
 
-fun gameBoard(p1: String = "", p2: String = ""){
+fun gameBoard(p1: String? = "", cpu: String? = ""){
 
     var firstRow = mutableListOf(" "," "," ")
     var secondRow = mutableListOf(" "," "," ")
     var thirdRow = mutableListOf(" "," ", " ")
-
+    // if a number goes missing, the next turn can not select that slot.
+    var gameBoardRef = mutableListOf(1,2,3,4,5,6,7,8,9)
 
 
 

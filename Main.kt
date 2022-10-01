3ee5848daf5b1a7bc540ae4a4123879c89425cec
AJ <63066039+ajworks13@users.noreply.github.com>
2022-10-01 18:55:59 +0000

    Update Main.kt
    Made the lists global for now? It is working this way instead of locally. Have to add a try catch to stop duplication and anything outside of 1-9.

--- Main.kt
+++ Main.kt
@@ -1,3 +1,14 @@
+// global area
+
+// determines winning or draw patterns.
+var firstRow = mutableListOf(" "," "," ")
+var secondRow = mutableListOf(" "," "," ")
+var thirdRow = mutableListOf(" "," ", " ")
+
+// Keeps track of which ones can not be used after it was already chosen
+var gameBoardRef = mutableListOf(1,2,3,4,5,6,7,8,9)
+//-----------------------------------------------------------------------------
+
 fun main() {
 
     var boss: String? = null // empty for now
@@ -30,17 +41,12 @@ fun main() {
 } // main function
 
 fun gameBoard(p1: String? = "", cpu: String? = ""){
-    // using these lists to track a winning or draw pattern.
-    var firstRow = mutableListOf(" "," "," ")
-    var secondRow = mutableListOf(" "," "," ")
-    var thirdRow = mutableListOf(" "," ", " ")
-
-    // if a number goes missing, the next turn can not select that slot.
-    var gameBoardRef = mutableListOf(1,2,3,4,5,6,7,8,9)
     println(gameBoardRef)
+    // removes the selected slot.
+    if(p1 == "1"){gameBoardRef.removeAt(0)}
+
     when(p1){
         "1" -> firstRow.add(0, "X")
-        "1" -> gameBoardRef.removeAt(0)
         "2" -> firstRow.add(1, "X")
         "3" -> firstRow[2] = "X"
         "4" -> secondRow[0] = "X"
@@ -54,7 +60,7 @@ fun gameBoard(p1: String? = "", cpu: String? = ""){
                     var corrected = false
 
                     println("Invalid input. Try 1 through 9 if it is available.")
-                    var outOfBounce = readLine()!!.toInt()
+                    var outOfBounce = readLine()!!.toInt() // converting to int
 // LEFT OFF HERE.....................................
                     if(outOfBounce <= 1 || outOfBounce >= 9){
                         corrected = true

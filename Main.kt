33de59413449e7f7d47ed33324e4fff2f9cdf170
AJ <63066039+ajworks13@users.noreply.github.com>
2022-10-03 16:10:25 +0000

    Update Main.kt
    Fixed the way I am checking with the ref function. By removing the elements, it was causing index exception errors.

--- Main.kt
+++ Main.kt
@@ -6,7 +6,7 @@ var secondRow = mutableListOf(" "," "," ")
 var thirdRow = mutableListOf(" "," ", " ")
 
 // Keeps track of which ones can not be used after it was already chosen
-var gameBoardRef = mutableListOf(1,2,3,4,5,6,7,8,9,0,0)
+var gameBoardRef = mutableListOf(1,2,3,4,5,6,7,8,9)
 
 var count = 0
 //-----------------------------------------------------------------------------
@@ -64,22 +64,34 @@ fun main() {
 
 fun regulator(p1: String? = null, cpu: String? = null){
     println("The count $count")
-    if(count == 1 && p1 == "1" || cpu == "1"){
+    if(count >= 1 && p1 == "1" && gameBoardRef[0] == 0){
         println("ALREADY SELECTED")
+        do{
+            println("Try another unselected number: ")
+            println(gameBoardRef)
+            var p1Chance = readLine()
+
+            if(p1Chance != "1"){
+                break
+            }
+        }while(true)
+
+    }else if(count >= 1 && cpu == "1" && gameBoardRef[0] == 0){
+        println("To cpu: ALREADY SELECTED!")
     }
 
     if(p1 == "1" || cpu == "1" && gameBoardRef.contains(0)){
-        gameBoardRef.removeAt(0)
+        gameBoardRef[0] = 0
         count++
     }
-    if(p1 == "2" || cpu == "2"){gameBoardRef.removeAt(1)}
-    if(p1 == "3" || cpu == "3"){gameBoardRef.removeAt(2)}
-    if(p1 == "4" || cpu == "4"){gameBoardRef.removeAt(3)}
-    if(p1 == "5" || cpu == "5"){gameBoardRef.removeAt(4)}
-    if(p1 == "6" || cpu == "6"){gameBoardRef.removeAt(5)}
-    if(p1 == "7" || cpu == "7"){gameBoardRef.removeAt(6)}
-    if(p1 == "8" || cpu == "8"){gameBoardRef.removeAt(7)}
-    if(p1 == "9" || cpu == "9"){gameBoardRef.removeAt(8)}
+    if(p1 == "2" || cpu == "2"){gameBoardRef[1] = 0}
+    if(p1 == "3" || cpu == "3"){gameBoardRef[2] = 0}
+    if(p1 == "4" || cpu == "4"){gameBoardRef[3] = 0}
+    if(p1 == "5" || cpu == "5"){gameBoardRef[4] = 0}
+    if(p1 == "6" || cpu == "6"){gameBoardRef[5] = 0}
+    if(p1 == "7" || cpu == "7"){gameBoardRef[6] = 0}
+    if(p1 == "8" || cpu == "8"){gameBoardRef[7] = 0}
+    if(p1 == "9" || cpu == "9"){gameBoardRef[8] = 0}
 
 // LEFT OFF HERE...................................
 
